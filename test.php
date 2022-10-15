1e66337be4d0f529d2ef50d5cb6070ee75a8c366
Yoga838 <212410102031@mail.unej.ac.id>
2022-10-15 15:08:03 +0700

    new tugas 07 appear

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+$number = 999999;
+echo number_format($number,0,"",".");
+echo "\n";
+
+//number_format(number,decimals,decimalpoint,separator) 
+
+
+
+
+<?php
+
+// $begin = new DateTime( "2022-01-01" );
+// $end   = new DateTime( "2015-07-09" );
+
+// for($i = $begin; $i <= $end; $i->modify('+1 day')){
+//     echo $i->format("Y-m-d");
+// }
+// $temp = $begin;
+// for($i =1; $i <=1000; $i++){
+//     echo $temp->format("d-F-Y");
+//     echo "\n";
+//     $temp->modify('+1 day');
+// }
+
+// for ($i=1;$i<=999;$i++){
+
+// }
+// $invID = str_pad($invID, 4, '0', STR_PAD_LEFT);
+?>
---
+++ tugas07.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 07</title>
+    <style>
+        h1{
+            margin-top:30px;
+            margin-left:40px
+        }
+        tr,td,th {
+            padding :10px;
+        }
+        .ratakiri{
+            text-align: left;
+        }
+        .st{
+            padding-right:5em;
+        }
+        .du{   
+            padding-right:15em;
+        }
+        .tg{
+            padding-right:20em;
+        }
+        .ratakanan{
+            text-align:right;
+            padding-left: 10em;
+        }
+    </style>
+</head>
+<body>
+    <h1>Praktikum 07 - practical PHP</h1>
+    <center><table  border = 1 solid >
+        <tr>
+            <th class="ratakiri st">No.</th>
+            <th class="ratakiri du">Tanggal</th>
+            <th class="ratakiri tg">Keterangan</th>
+            <th class="ratakanan">Jumlah</th>
+        </tr>
+        <?php $begin = new DateTime( "2022-01-01" );
+              $temp = $begin; 
+              $num = 1000; 
+              for ($i=1;$i<=999;$i++){
+                $amount = number_format($num,0,"","."); 
+                $digit = str_pad($i, 3, '0', STR_PAD_LEFT); ?>
+            <tr>
+                <td class="ratakiri"><?php echo  $i;?></td>
+                <td class="ratakiri"><?php echo $temp->format("j-F-Y"); ?></td>
+                <?php $date = $temp->format("Y-m-d"); ?>
+                <?php $temp->modify('+1 day'); ?>
+                <td class="ratakiri"><?php echo "TRN-$date-$digit"; ?></td>
+                <td class="ratakanan"><?php echo "Rp $amount"; ?></td>
+                <?php $num += 1000; ?>
+            </tr>
+        <?php } ?>
+    </table></center>
+    <center><h3>{212410102031}-{Ahmad Bagus Prayoga}</h3></center>
+</body>
+</html>
